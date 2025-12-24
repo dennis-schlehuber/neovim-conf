a6638852c661a04bd4461010dc4203b648a0ae10
extrain-dennis <dennis@extrain.io>
2025-12-24 11:55:12 +0100

    FIX: inlie blame update

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -22,12 +22,12 @@ require('gitsigns').setup({
     current_line_blame_opts = {
       virt_text = true,
       virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-      delay = 5000,
+      delay = 200,
       ignore_whitespace = false,
     },
     current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
     sign_priority = 6,
-    update_debounce = 100,
+    update_debounce = 10,
     status_formatter = nil, -- Use default
     max_file_length = 40000, -- Disable if file is longer than this (in lines)
     preview_config = {
