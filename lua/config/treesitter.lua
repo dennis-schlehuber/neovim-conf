b4b48b0aaf39e25b1dc8ab99768ccf6c04d7c29a
Dennis Schlehuber <dennis@Denniss-MacBook-Air.local>
2026-07-02 14:42:20 +0200

    feat(-): minor improvements

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,12 +1,8 @@
-require('nvim-treesitter.configs').setup({
+require('nvim-treesitter').setup({
   ensure_installed = {
     'vimdoc', 'javascript', 'typescript', 'c', 'lua', 'rust',
     'jsdoc', 'bash', 'java', 'kotlin', 'svelte', 'html', 'css', 'go',
     'python',
   },
-  sync_install = false,
   auto_install = true,
-  highlight = {
-    enable = true,
-  },
 })
